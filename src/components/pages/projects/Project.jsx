efd0ccf4e09fb58342ce60a91301f1f51b84bd3f
PhilipVigus <philipvigus@yahoo.co.uk>
2022-03-20 17:50:35 +0000

    Add project technologies

--- src/components/pages/projects/Project.jsx
+++ src/components/pages/projects/Project.jsx
@@ -6,6 +6,26 @@ function Project({ data }) {
     <Box p={10} w="75%" mx="auto">
       <h2>{data.title}</h2>
       <Text>{data.description}</Text>
+      <Box>
+        {data.technologies.languages.map((language) => (
+          <Text key={language}>{language}</Text>
+        ))}
+        {data.technologies.frontend.map((framework) => (
+          <Text key={framework}>{framework}</Text>
+        ))}
+        {data.technologies.backend.frameworks.map((framework) => (
+          <Text key={framework}>{framework}</Text>
+        ))}
+        {data.technologies.backend.databases.map((database) => (
+          <Text key={database}>{database}</Text>
+        ))}
+        {data.technologies.testing.map((framework) => (
+          <Text key={framework}>{framework}</Text>
+        ))}
+        {data.technologies.ci_cd.map((solution) => (
+          <Text key={solution}>{solution}</Text>
+        ))}
+      </Box>
     </Box>
   );
 }
@@ -13,7 +33,17 @@ function Project({ data }) {
 Project.propTypes = {
   data: PropTypes.shape({
     title: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired
+    description: PropTypes.string.isRequired,
+    technologies: PropTypes.shape({
+      languages: PropTypes.arrayOf(PropTypes.string).isRequired,
+      frontend: PropTypes.arrayOf(PropTypes.string).isRequired,
+      backend: PropTypes.shape({
+        frameworks: PropTypes.arrayOf(PropTypes.string).isRequired,
+        databases: PropTypes.arrayOf(PropTypes.string).isRequired
+      }).isRequired,
+      testing: PropTypes.arrayOf(PropTypes.string).isRequired,
+      ci_cd: PropTypes.arrayOf(PropTypes.string).isRequired
+    }).isRequired
   }).isRequired
 };
 
--- src/tests/Project.test.jsx
+++ src/tests/Project.test.jsx
@@ -4,13 +4,29 @@ import Project from '../components/pages/projects/Project';
 describe('Project Component', () => {
   it('Renders the Project component', () => {
     const testData = {
-      title: 'test title',
-      description: 'test description'
+      title: 'title',
+      description: 'description',
+      technologies: {
+        languages: ['language'],
+        frontend: ['frontend framework'],
+        backend: {
+          frameworks: ['backend framework'],
+          databases: ['database']
+        },
+        testing: ['testing framework'],
+        ci_cd: ['ci/cd solution']
+      }
     };
 
     render(<Project data={testData} />);
 
     expect(screen.getByText(testData.title)).toBeInTheDocument();
     expect(screen.getByText(testData.description)).toBeInTheDocument();
+    expect(screen.getByText(testData.technologies.languages[0])).toBeInTheDocument();
+    expect(screen.getByText(testData.technologies.frontend[0])).toBeInTheDocument();
+    expect(screen.getByText(testData.technologies.backend.frameworks[0])).toBeInTheDocument();
+    expect(screen.getByText(testData.technologies.backend.databases[0])).toBeInTheDocument();
+    expect(screen.getByText(testData.technologies.testing[0])).toBeInTheDocument();
+    expect(screen.getByText(testData.technologies.ci_cd[0])).toBeInTheDocument();
   });
 });
