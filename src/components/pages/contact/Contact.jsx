921ab4de0a90d89594c6b6fbe976ceee9a159df1
PhilipVigus <philipvigus@yahoo.co.uk>
2022-03-20 18:51:15 +0000

    Install emailjs package

--- src/components/pages/contact/Contact.jsx
+++ src/components/pages/contact/Contact.jsx
@@ -1,5 +1,7 @@
-const Contact = () => {
-  return 'Contact';
-};
+import { Box } from '@chakra-ui/react';
+
+function Contact() {
+  return <Box>{process.env.REACT_APP_EMAILJS_USER_ID}</Box>;
+}
 
 export default Contact;
