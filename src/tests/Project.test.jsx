d6c76c4e1be0e308977b54a400c404213f787384
PhilipVigus <philipvigus@yahoo.co.uk>
2022-03-20 17:12:57 +0000

    Add test assertion for project description

--- src/tests/Project.test.jsx
+++ src/tests/Project.test.jsx
@@ -4,11 +4,13 @@ import Project from '../components/pages/projects/Project';
 describe('Project Component', () => {
   it('Renders the Project component', () => {
     const testData = {
-      title: 'test title'
+      title: 'test title',
+      description: 'test description'
     };
 
     render(<Project data={testData} />);
 
     expect(screen.getByText(testData.title)).toBeInTheDocument();
+    expect(screen.getByText(testData.description)).toBeInTheDocument();
   });
 });
