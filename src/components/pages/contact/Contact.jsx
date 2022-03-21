6e2b6b1d6609e9e0a906de9ae96d90a5b5a6c7e0
PhilipVigus <philipvigus@yahoo.co.uk>
2022-03-21 18:14:31 +0000

    Add a contact form

--- src/components/pages/contact/Contact.jsx
+++ src/components/pages/contact/Contact.jsx
@@ -1,7 +1,88 @@
-import { Box } from '@chakra-ui/react';
+/* eslint-disable react/prop-types */
+import {
+  Button,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input,
+  Textarea
+} from '@chakra-ui/react';
+import { Field, Form, Formik } from 'formik';
+import emailjs from '@emailjs/browser';
 
 function Contact() {
-  return <Box>{process.env.REACT_APP_EMAILJS_USER_ID}</Box>;
+  const validateEmail = (email) => {
+    let error;
+
+    if (!email) {
+      error = 'Email is required';
+    }
+
+    return error;
+  };
+
+  const validateMessage = (message) => {
+    let error;
+
+    if (!message) {
+      error = 'Message is required';
+    }
+
+    return error;
+  };
+
+  return (
+    <Formik
+      initialValues={{ email: '', message: '' }}
+      onSubmit={(values, { setSubmitting }) => {
+        emailjs.init(process.env.REACT_APP_EMAILJS_USER_ID);
+
+        const emailParams = { user_email: values.email, message: values.message };
+
+        emailjs
+          .send(
+            process.env.REACT_APP_EMAILJS_SERVICE_ID,
+            process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+            emailParams
+          )
+          .then((response) => {
+            console.log('SUCCESS!', response.status, response.text);
+            setSubmitting(false);
+          })
+          .catch((error) => {
+            console.log('FAILED...', error);
+            setSubmitting(false);
+          });
+      }}>
+      {(props) => (
+        <Form>
+          <Field name="email" validate={validateEmail}>
+            {({ field, form }) => (
+              <FormControl isInvalid={form.errors.email && form.touched.email}>
+                <FormLabel htmlFor="email">Email</FormLabel>
+                <Input {...field} id="email" placeholder="Email" type="email" />
+                <FormErrorMessage>{form.errors.email}</FormErrorMessage>
+              </FormControl>
+            )}
+          </Field>
+
+          <Field name="message" validate={validateMessage}>
+            {({ field, form }) => (
+              <FormControl isInvalid={form.errors.message && form.touched.message}>
+                <FormLabel htmlFor="message">Message</FormLabel>
+                <Textarea {...field} id="message" placeholder="Message" type="text" />
+                <FormErrorMessage>{form.errors.message}</FormErrorMessage>
+              </FormControl>
+            )}
+          </Field>
+
+          <Button mt={4} isLoading={props.isSubmitting} type="submit" name="submit">
+            Submit
+          </Button>
+        </Form>
+      )}
+    </Formik>
+  );
 }
 
 export default Contact;
--- src/tests/Contact.test.jsx
+++ src/tests/Contact.test.jsx
@@ -1,8 +1,101 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import emailjs from '@emailjs/browser';
 import Contact from '../components/pages/contact/Contact';
 
-test('renders Contact page', () => {
-  render(<Contact />);
-  const linkElement = screen.getByText(/contact/i);
-  expect(linkElement).toBeInTheDocument();
+jest.mock('@emailjs/browser');
+
+emailjs.init = jest.fn();
+emailjs.send = jest.fn();
+
+describe('Contact Page', () => {
+  beforeAll(() => {
+    process.env.REACT_APP_EMAILJS_USER_ID = 'user_id';
+    process.env.REACT_APP_EMAILJS_SERVICE_ID = 'email_service_id';
+    process.env.REACT_APP_EMAILJS_TEMPLATE_ID = 'email_template_id';
+  });
+
+  it('renders Contact page', () => {
+    render(<Contact />);
+
+    expect(screen.getByText('Email')).toBeInTheDocument();
+  });
+
+  it('submits the form when you click the button if it is filled in correctly', async () => {
+    const TEST_EMAIL = 'test@email.com';
+    const TEST_MESSAGE = 'test message';
+
+    render(<Contact />);
+
+    userEvent.type(screen.getByLabelText('Email'), TEST_EMAIL);
+    userEvent.type(screen.getByLabelText('Message'), TEST_MESSAGE);
+
+    userEvent.click(screen.getByRole('button', { name: 'Submit' }));
+
+    await waitFor(() => {
+      expect(emailjs.init).toHaveBeenCalledTimes(1);
+      expect(emailjs.init.mock.calls[0]).toEqual([process.env.REACT_APP_EMAILJS_USER_ID]);
+
+      expect(emailjs.send).toHaveBeenCalledTimes(1);
+      expect(emailjs.send.mock.calls[0]).toEqual([
+        process.env.REACT_APP_EMAILJS_SERVICE_ID,
+        process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+        {
+          user_email: TEST_EMAIL,
+          message: TEST_MESSAGE
+        }
+      ]);
+    });
+  });
+
+  it('will not submit the form when you click the button if the email is blank', async () => {
+    const TEST_MESSAGE = 'test message';
+
+    render(<Contact />);
+
+    userEvent.type(screen.getByLabelText('Message'), TEST_MESSAGE);
+
+    userEvent.click(screen.getByRole('button', { name: 'Submit' }));
+
+    await waitFor(() => {
+      expect(emailjs.init).toHaveBeenCalledTimes(0);
+      expect(emailjs.send).toHaveBeenCalledTimes(0);
+
+      expect(screen.getByText('Email is required')).toBeInTheDocument();
+    });
+  });
+
+  it('will not submit the form when you click the button if the message is blank', async () => {
+    const TEST_EMAIL = 'test@email.com';
+
+    render(<Contact />);
+
+    userEvent.type(screen.getByLabelText('Email'), TEST_EMAIL);
+
+    userEvent.click(screen.getByRole('button', { name: 'Submit' }));
+
+    await waitFor(() => {
+      expect(emailjs.init).toHaveBeenCalledTimes(0);
+      expect(emailjs.send).toHaveBeenCalledTimes(0);
+
+      expect(screen.getByText('Message is required')).toBeInTheDocument();
+    });
+  });
+
+  it('will not submit the form when you click the button if the email is incorrectly formatted', async () => {
+    const TEST_EMAIL = 'testemail.com';
+    const TEST_MESSAGE = 'test message';
+
+    render(<Contact />);
+
+    userEvent.type(screen.getByLabelText('Email'), TEST_EMAIL);
+    userEvent.type(screen.getByLabelText('Message'), TEST_MESSAGE);
+
+    userEvent.click(screen.getByRole('button', { name: 'Submit' }));
+
+    await waitFor(() => {
+      expect(emailjs.init).toHaveBeenCalledTimes(0);
+      expect(emailjs.send).toHaveBeenCalledTimes(0);
+    });
+  });
 });
