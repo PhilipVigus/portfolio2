3101b376894fe46ecdfc83a7676db0ab73609c7b
PhilipVigus <philipvigus@yahoo.co.uk>
2022-03-22 19:49:57 +0000

    Add some test data

--- src/components/pages/loading/Loading.jsx
+++ src/components/pages/loading/Loading.jsx
@@ -2,9 +2,13 @@ import { Box } from '@chakra-ui/react';
 import TypedSentence from './TypedSentence';
 
 function Loading() {
+  const onFinishTyping = () => {
+    console.log('finished');
+  };
+
   return (
     <Box p={10} w="75%" mx="auto">
-      <TypedSentence sentence="test" />
+      <TypedSentence sentence="test" onFinishTyping={onFinishTyping} />
     </Box>
   );
 }
