8a4a68b0cb815d29f1385b462c876157b2ff0b3c
PhilipVigus <philipvigus@yahoo.co.uk>
2022-03-23 17:24:39 +0000

    Make the cursor flash

--- src/components/pages/loading/Cursor.jsx
+++ src/components/pages/loading/Cursor.jsx
@@ -1,7 +1,23 @@
 import { Text } from '@chakra-ui/react';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function Cursor() {
+  const FLASH_TIME = 350;
+
+  const [isShowing, setIsShowing] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsShowing(!isShowing);
+    }, FLASH_TIME);
+
+    return () => clearTimeout(timer);
+  }, [isShowing]);
+
+  if (!isShowing) {
+    return null;
+  }
+
   return <Text>&#9608;</Text>;
 }
 
