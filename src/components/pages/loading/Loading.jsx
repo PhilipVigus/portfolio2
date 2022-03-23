18c161c59cca509467f5926e30b2acf8f7c96cea
PhilipVigus <philipvigus@yahoo.co.uk>
2022-03-23 17:31:37 +0000

    Move cursor to loading and add start delay

--- src/components/pages/loading/Loading.jsx
+++ src/components/pages/loading/Loading.jsx
@@ -1,14 +1,38 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Flex } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+import CommandPrompt from './CommandPrompt';
+import Cursor from './Cursor';
+// import SystemMessage from './SystemMessage';
 import TypedSentence from './TypedSentence';
 
 function Loading() {
+  const DELAY_BEFORE_TYPING = 800;
+
+  const [isTyping, setIsTyping] = useState(false);
+  const [showCursor, setShowCursor] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsTyping(true);
+    }, DELAY_BEFORE_TYPING);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   const onFinishTyping = () => {
-    console.log('finished');
+    setShowCursor(false);
   };
 
   return (
     <Box p={10} w="75%" mx="auto">
-      <TypedSentence sentence="test" onFinishTyping={onFinishTyping} />
+      <Flex>
+        <CommandPrompt />
+        {isTyping && (
+          <TypedSentence sentence="load awesome portfolio" onFinishTyping={onFinishTyping} />
+        )}
+        {showCursor && <Cursor />}
+      </Flex>
+      {/* <SystemMessage message="test" onFinishDisplay={onFinishTyping} numberOfDots={15} /> */}
     </Box>
   );
 }
--- src/components/pages/loading/TypedSentence.jsx
+++ src/components/pages/loading/TypedSentence.jsx
@@ -1,12 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import Cursor from './Cursor';
 
 function TypedSentence({ sentence, onFinishTyping }) {
   const TYPING_DELAY = 130;
 
   const [charactersTyped, setCharactersTyped] = useState(0);
-  const [showCursor, setShowCursor] = useState(true);
 
   useEffect(() => {
     if (charactersTyped < sentence.length) {
@@ -17,18 +15,12 @@ function TypedSentence({ sentence, onFinishTyping }) {
       return () => clearTimeout(timer);
     }
 
-    setShowCursor(false);
     onFinishTyping();
 
     return () => {};
   }, [onFinishTyping, sentence.length, charactersTyped]);
 
-  return (
-    <>
-      {sentence.slice(0, charactersTyped)}
-      {showCursor && <Cursor />}
-    </>
-  );
+  return <>{sentence.slice(0, charactersTyped)}</>;
 }
 
 TypedSentence.propTypes = {
