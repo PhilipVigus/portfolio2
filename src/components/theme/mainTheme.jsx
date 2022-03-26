5fc47fdad2636afbd94592519751cfd4b1d14122
PhilipVigus <philipvigus@yahoo.co.uk>
2022-03-26 17:20:54 +0000

    Add the main theme

---
+++ src/components/theme/mainTheme.jsx
@@ -0,0 +1,13 @@
+import { extendTheme } from '@chakra-ui/react';
+
+const mainTheme = extendTheme({
+  colors: {
+    dark: '#2E3240',
+    light: '#EDE4D7',
+    mid: '#65626A',
+    lightAccent: '#8A5541',
+    darkAccent: '#614342'
+  }
+});
+
+export default mainTheme;
