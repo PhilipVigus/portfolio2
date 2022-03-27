3b46092f934ce7481d4f3d50ac148140a778c44a
PhilipVigus <philipvigus@yahoo.co.uk>
2022-03-27 13:38:50 +0100

    Add transition animations

--- src/components/pages/about/About.jsx
+++ src/components/pages/about/About.jsx
@@ -1,36 +1,76 @@
-import { Box, ListItem, UnorderedList, Text } from '@chakra-ui/react';
-import SectionHeading from './SectionHeading';
+import { Box, ListItem, UnorderedList, Text, Heading } from '@chakra-ui/react';
+import { motion } from 'framer-motion';
+
+const AnimatedBox = motion(Box);
+const AnimatedText = motion(Text);
+const AnimatedList = motion(UnorderedList);
+
+const containerVariants = {
+  hidden: {
+    opacity: 0,
+    scale: 0.8
+  },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      duration: 0.2,
+      delay: 0.2,
+      when: 'beforeChildren',
+      staggerChildren: 0.1
+    }
+  }
+};
+
+const itemVariants = {
+  hidden: {
+    opacity: 0,
+    y: 10
+  },
+  visible: {
+    opacity: 1,
+    y: 0
+  }
+};
 
 function About() {
   return (
-    <Box p={10} w="75%" mx="auto">
+    <AnimatedBox
+      p={10}
+      w="75%"
+      mx="auto"
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible">
       <Box bg="light" p="8" rounded="lg">
-        <SectionHeading>About Me</SectionHeading>
-        <Text mt={2}>
+        <Heading variant="section">About Me</Heading>
+        <AnimatedText mt={2} variants={itemVariants}>
           After qualifying with a degree in Computer Science, I spent the early part of my career in
           the tech industry, initially as a developer before moving into business development. I
           became an ICT teacher before taking a role in educational quality assurance.
-        </Text>
+        </AnimatedText>
 
-        <Text mt={4}>
+        <AnimatedText mt={4} variants={itemVariants}>
           About two and a half years a go I changed direction, returning to my roots in software
           development. I completed the Maker&apos;s Academy boot camp, and soon after secured a role
           as developer with Mumsnet where I still work. I have been heavily involved in the
           migration of the company&apos;s website from a legacy Java implementation to a new
           codebase using the TALL stack (Tailwind, Alpine, Livewire and Laravel).
-        </Text>
+        </AnimatedText>
 
-        <Text mt={4}>
+        <AnimatedText mt={4} variants={itemVariants}>
           In addition to the technical knowledge required to work as a developer, I have excellent
           communication and general interpersonal skills from the various client-facing roles I have
           held in the past. I love the blend of creative and analytical skills required from a
           developer, and seeing an application grow before my eyes really excites me. I am happiest
           when I&apos;m learning, and the constantly changing tech landscape means that there will
           always be something new to understand.
-        </Text>
+        </AnimatedText>
 
-        <Text mt={4}>My responsibilities at Mumsnet have included:</Text>
-        <UnorderedList>
+        <AnimatedText mt={4} variants={itemVariants}>
+          My responsibilities at Mumsnet have included:
+        </AnimatedText>
+        <AnimatedList variants={itemVariants}>
           <ListItem>
             Day-to-day lead of the project rebuilding the administrative functions of the site
           </ListItem>
@@ -47,9 +87,9 @@ function About() {
             Optimising SQL scripts to speed up the migration of millions of records to a new
             database
           </ListItem>
-        </UnorderedList>
+        </AnimatedList>
       </Box>
-    </Box>
+    </AnimatedBox>
   );
 }
 
