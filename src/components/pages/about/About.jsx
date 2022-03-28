7f16d1a3261f41b597c31969da75f3a8763a3292
PhilipVigus <philipvigus@yahoo.co.uk>
2022-03-28 18:17:28 +0100

    Delete unnecessary Box component

--- src/components/pages/about/About.jsx
+++ src/components/pages/about/About.jsx
@@ -17,42 +17,40 @@ function About() {
       variants={parentVariants}
       initial="hidden"
       animate="visible">
-      <Box bg="light" p="8" rounded="lg">
-        <Heading variant="section">About Me</Heading>
-        <AnimatedText mt={2} variants={childVariants}>
-          After qualifying with a degree in Computer Science, I spent the early part of my career in
-          the tech industry, initially as a developer before moving into business development. I
-          then became an ICT teacher before taking a role in educational quality assurance.
-        </AnimatedText>
+      <Heading variant="section">About Me</Heading>
+      <AnimatedText mt={2} variants={childVariants}>
+        After qualifying with a degree in Computer Science, I spent the early part of my career in
+        the tech industry, initially as a developer before moving into business development. I then
+        became an ICT teacher before taking a role in educational quality assurance.
+      </AnimatedText>
 
-        <AnimatedText mt={4} variants={childVariants}>
-          About two and a half years a go I changed direction, returning to my roots in software
-          development. I completed the Maker&apos;s Academy boot camp, and soon after secured a role
-          as a developer with Mumsnet where I still work. I have been heavily involved in the
-          migration of the company&apos;s website from a legacy Java implementation to a new
-          codebase using the TALL stack (Tailwind, Alpine, Livewire and Laravel), and was promoted
-          to mid-level developer within a year of working there.
-        </AnimatedText>
+      <AnimatedText mt={4} variants={childVariants}>
+        About two and a half years a go I changed direction, returning to my roots in software
+        development. I completed the Maker&apos;s Academy boot camp, and soon after secured a role
+        as a developer with Mumsnet where I still work. I have been heavily involved in the
+        migration of the company&apos;s website from a legacy Java implementation to a new codebase
+        using the TALL stack (Tailwind, Alpine, Livewire and Laravel), and was promoted to mid-level
+        developer within a year of working there.
+      </AnimatedText>
 
-        <AnimatedText mt={4} variants={childVariants}>
-          My technical skills include:
-        </AnimatedText>
+      <AnimatedText mt={4} variants={childVariants}>
+        My technical skills include:
+      </AnimatedText>
 
-        <AnimatedList>
-          {data.skills.map((skill) => (
-            <SkillListItem key={skill.title} skillList={skill} />
-          ))}
-        </AnimatedList>
+      <AnimatedList>
+        {data.skills.map((skill) => (
+          <SkillListItem key={skill.title} skillList={skill} />
+        ))}
+      </AnimatedList>
 
-        <AnimatedText mt={4} variants={childVariants}>
-          In addition to the technical knowledge required to work as a developer, I have excellent
-          communication and general interpersonal skills from the various client-facing roles I have
-          held in the past. I love the blend of creative and analytical skills required to be a
-          developer, and seeing an application grow before my eyes really excites me. I am happiest
-          when I&apos;m learning, and the constantly changing tech landscape means that there will
-          always be something new to understand.
-        </AnimatedText>
-      </Box>
+      <AnimatedText mt={4} variants={childVariants}>
+        In addition to the technical knowledge required to work as a developer, I have excellent
+        communication and general interpersonal skills from the various client-facing roles I have
+        held in the past. I love the blend of creative and analytical skills required to be a
+        developer, and seeing an application grow before my eyes really excites me. I am happiest
+        when I&apos;m learning, and the constantly changing tech landscape means that there will
+        always be something new to understand.
+      </AnimatedText>
     </AnimatedBox>
   );
 }
