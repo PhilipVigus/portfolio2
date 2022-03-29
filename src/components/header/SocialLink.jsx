754e27b0fbdc67430f2a434fa1ffb83323fe1160
PhilipVigus <philipvigus@yahoo.co.uk>
2022-03-29 17:36:37 +0100

    Improve visual feedback for links

--- src/components/header/SocialLink.jsx
+++ src/components/header/SocialLink.jsx
@@ -3,7 +3,14 @@ import PropTypes from 'prop-types';
 
 function SocialLink({ children, href }) {
   return (
-    <Button as={Link} bg="mid" href={href} target="_blank" _hover={{ bg: 'mid' }} p={0} m={0}>
+    <Button
+      as={Link}
+      bg="mid"
+      href={href}
+      target="_blank"
+      _hover={{ bg: 'mid', color: 'orange.300' }}
+      p={0}
+      m={0}>
       {children}
     </Button>
   );
